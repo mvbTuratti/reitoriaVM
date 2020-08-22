95fbb05bb71e927f34abe8cfdda82e25c7efe24a
Matheus Turatti <59181759+mvbTuratti@users.noreply.github.com>
2020-08-22 01:28:16 -0300

    Update __init__.py

--- flaskapp/__init__.py
+++ flaskapp/__init__.py
@@ -6,6 +6,7 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'e59d86e807f40bafb20cca686aebdd04'
 #essa configuração seta a localização do banco de dados, aqui é definido que é localmente um arquivo de saída com nome 'site.db' no mesmo repositório deste __init__.py
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 2
 
 #criação da variável que acessa o banco de dados
 db = SQLAlchemy(app)
